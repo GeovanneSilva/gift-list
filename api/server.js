b03c9c7fd8fbee17e20f04199b12397b81d2bd42
Geovanne Silva <66743857+GeovanneSilva@users.noreply.github.com>
2024-09-28 16:40:58 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/blog/:resource/:id/show': '/Sugestão/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
